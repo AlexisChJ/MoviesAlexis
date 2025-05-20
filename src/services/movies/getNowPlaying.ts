4247b28bd1a81ddde83f6a2248a30e2d6fab4215
AlexisChJ <alexchj1917@gmail.com>
2025-05-19 21:59:06 -0600

    quinto intento

--- src/services/movies/getNowPlaying.ts
+++ src/services/movies/getNowPlaying.ts
@@ -1,13 +1,13 @@
 import api from "../api";
 
 export const getNowPlayingMovies = async (page: number = 1) => {
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   let res: any;
   const endpoint = `/movie/now_playing?language=en-US&page=${page}`;
 
   try {
     const response = await api.get(endpoint);
     res = response.data;
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
   } catch (err: any) {
     res = err.response;
   }
--- src/services/movies/getTopRated.ts
+++ src/services/movies/getTopRated.ts
@@ -1,13 +1,13 @@
 import api from "../api";
 
 export const getTopRatedMovies = async (page: number = 1) => {
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
   let res: any;
   const endpoint = `/movie/top_rated?language=en-US&page=${page}`;
 
   try {
     const response = await api.get(endpoint);
     res = response.data;
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
   } catch (err: any) {
     res = err.response;
   }
